9d9beeb00170d09e4c9e9ff822401763741e4670
H82M19\Student <Student>
2025-04-16 10:09:59 +0700

    done usereducer

---
+++ dung-22684211/baitap/Usereducer.jsx
@@ -0,0 +1,29 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case '+':
+      return { count: state.count + 1 };
+    case '-':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function Usereducer() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div style={{backgroundColor: "lightblue", width:"400px", borderRadius: "10px"}}>
+      <h1>{state.count}</h1>
+      <button style={{backgroundColor: "green",width:"100px"}} onClick={() => dispatch({ type: '+' })}>+</button>
+      <button style={{backgroundColor: "red", width:"100px"}} onClick={() => dispatch({ type: '-' })}>-</button>
+    </div>
+  );
+}
+
+
+export default Usereducer
---
+++ dung-22684211/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Usereducer from "../baitap/Usereducer"
+
+function App() {
+
+
+  return (
+    <>
+      <Usereducer></Usereducer>
+
+
+    </>
+  )
+}
+
+export default App
